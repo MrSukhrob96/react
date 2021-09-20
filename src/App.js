4d97296f5eeed3467d097251041a343be835e545
MrSukhrob96 <sukhrobazimov1996@gmail.com>
2021-09-20 08:35:16 +0500

    react initilize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Item from "./Item"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      posts: [
+        {
+          title: "Hello",
+          body: "Hello to someone"
+        }, {
+          title: "Good Day",
+          body: "godd day everyone"
+        }
+      ]
+    };
+  }
+
+  handleItemBtnClick = (id) => {
+    let item = this.state.posts.find((item, itemid) => {
+      return itemid === id
+    })
+
+    this.setState(() => ({
+      title: item.body
+    }))
+  }
+
+  inputHandle = (event) => {
+    this.setState((state) => (
+      {
+        title: event.target.value
+      }))
+  }
+
+  handleItemBtnDelete = (id) => {
+    // const item = this.state.posts.filter((item, itemid) => {
+    //   return itemid !== id
+    // })
+
+    const idx = this.state.posts.findIndex((el, elid) => elid === id);
+
+    const item = [
+      ...this.state.posts.splice(0, idx),
+      ...this.state.posts.splice(idx + 1)
+    ]
+
+    this.setState((state) => ({
+      posts: item
+    }))
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.inputHandle} />
+        <h3>{this.state.title}</h3>
+        {this.state.posts.map((item, id) => {
+          return <Item {...item} itemBtnOnClick={() => this.handleItemBtnClick(id)} itemBtnOnDelete={() => this.handleItemBtnDelete(id)} key={id} />
+        })}
+        <button>{this.state.title === '' ? 'Empty' : 'Not Empty'}</button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
